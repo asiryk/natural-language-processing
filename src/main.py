fb0bf962d65055a3c9e7974b21215df41972f83a
asiryk <asiryk.net@gmail.com>
2022-05-08 17:35:19 +0300

    chore: create src folder

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from conllu import parse_incr
 
 
 def print_parsed():
-    data_file = open("./ud_ukrainian/uk_iu-ud-dev.conllu", "r", encoding="utf-8")
+    data_file = open("../ud_ukrainian/uk_iu-ud-dev.conllu", "r", encoding="utf-8")
     parsed_file = list(parse_incr(data_file))
     print(parsed_file[:3])
 
