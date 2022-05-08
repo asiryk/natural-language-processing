e65573a41a3e60ee669420fa5635c9bbe82d09b2
asiryk <asiryk.net@gmail.com>
2022-05-08 21:25:27 +0300

    chore: play with tokens

--- src/main.py
+++ src/main.py
@@ -8,8 +8,10 @@ from src.paths import UK_TOKENS_PATH
 def print_parsed():
     data_file = open(UK_TOKENS_PATH, "r", encoding="utf-8")
     parsed_file = list(parse_incr(data_file))
-    print(parsed_file[:3])
+    sentence_tokens = parsed_file[0]
+    word_token = sentence_tokens[0]
+    print(word_token["upos"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print_parsed()
