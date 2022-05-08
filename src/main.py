1d4b979bd3dd70232d668489bbd42b6f45886808
asiryk <asiryk.net@gmail.com>
2022-05-08 18:38:36 +0300

    fix: use correct path after renaming. use separate file for path constants

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from io import open
+
 from conllu import parse_incr
+
+from src.paths import UK_TOKENS_PATH
 
 
 def print_parsed():
-    data_file = open("../ud_ukrainian/uk_iu-ud-dev.conllu", "r", encoding="utf-8")
+    data_file = open(UK_TOKENS_PATH, "r", encoding="utf-8")
     parsed_file = list(parse_incr(data_file))
     print(parsed_file[:3])
 
---
+++ src/paths.py
@@ -0,0 +1,1 @@
+UK_TOKENS_PATH = "./ud_ukrainian/uk_iu-ud-dev.conllu"
