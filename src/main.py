665a33fdd6e2f2e84781c72e8354268ee0d9967c
asiryk <asiryk.net@gmail.com>
2022-05-16 14:57:27 +0300

    feat: implement Signature type collect the data in a single class and implement serialization of the signature

--- src/main.py
+++ src/main.py
@@ -6,14 +6,14 @@ from src.model import traverse
 from src.paths import UK_TOKENS_PATH
 
 
-def print_parsed():
+def main():
     data_file = open(UK_TOKENS_PATH, "r", encoding="utf-8")
     parsed_file = list(parse_incr(data_file))
     print(len(parsed_file))
 
-    tree, _ = traverse(parsed_file[0].to_tree())
-    print(tree.get_token("upos"), tree.depth, tree.height)
+    tree = traverse(parsed_file[0].to_tree())
+    print(tree.signature)
 
 
 if __name__ == "__main__":
-    print_parsed()
+    main()
--- src/model.py
+++ src/model.py
@@ -1,45 +1,78 @@
+from enum import Enum
 from typing import List, Optional, Tuple
 
 from conllu import TokenTree
 from conllu.models import Token, Metadata
 
 
-class ParseTree:
-    token: Token
-    metadata: Optional[Metadata]
-    children: List['ParseTree']
+class SigType(Enum):
+    D = "d"  # one ancestor
+    W = "w"  # multiple ancestors
+    N = "n"  # no ancestors
 
-    parent: Optional['ParseTree']
+
+class Signature:
+    type: SigType
+    child_type: Optional[SigType]
     depth: int  # depth counting from the root element
     height: int  # length of the longest downward path to a leaf from that node
+    node: Tuple[str, str]  # DEPREL, UPOS
+    children_nodes: List[Tuple[str, str]]
+    sentence_id: Optional[str]
 
-    def __init__(self, token_node: TokenTree, children: List['ParseTree'], depth: int, height: int):
-        self.token = token_node.token
-        self.metadata = token_node.metadata
-        self.children = children
-
+    def __init__(self, current: 'ParseTree', children: List['ParseTree'], depth: int, height: int, sentence_id: str):
+        self.node = (current.get_token("deprel"), current.get_token("upos"))
+        self.children_nodes = [child.signature.node for child in children]
         self.depth = depth
         self.height = height
-        self.parent = None
+        self.sentence_id = sentence_id
+        if len(children) == 0:
+            self.type = SigType.N
+        elif len(children) == 1:
+            self.type = SigType.D
+        else:
+            self.type = SigType.W
 
-    def set_parent(self, parent: 'ParseTree'):
-        self.parent = parent
+    def __str__(self):
+        def str_node(n: Tuple[str, str]):
+            return f"{n[0]}+{n[1]}"
+
+        node = str_node(self.node)
+        children_nodes = ", ".join(str_node(child) for child in self.children_nodes)
+        return f"{self.type.value}({node}, {children_nodes}, {self.depth}, {self.height}, {self.sentence_id})"
+
+
+class ParseTree:
+    token: Token
+    metadata: Metadata
+    children: List['ParseTree']
+    signature: Signature
+
+    def __init__(self, token_node: TokenTree, children: List['ParseTree'], depth: int, height: int,
+                 metadata: Metadata):
+        self.token = token_node.token
+        self.metadata = metadata
+        self.children = children
+        self.signature = Signature(self, self.children, depth, height, metadata.get("sent_id"))
 
     def get_token(self, key: str):
         return self.token.get(key)
 
 
-def traverse(root: TokenTree, depth: int = 0) -> Tuple[ParseTree, int]:
+def _traverse(root: TokenTree, depth: int = 0, metadata: Optional[Metadata] = None) -> Tuple[ParseTree, int]:
+    metadata = root.metadata if metadata is None else metadata
     children = []
     height = 0
 
     for node in root.children:
-        data_node, node_subtree_depth = traverse(node, depth + 1)
+        data_node, node_subtree_depth = _traverse(node, depth + 1, metadata)
         height = max(height, node_subtree_depth + 1)
         children.append(data_node)
 
-    data_root = ParseTree(root, children, depth, height)
-    for node in children:
-        node.set_parent(data_root)
+    data_root = ParseTree(root, children, depth, height, metadata)
 
     return data_root, height
+
+
+def traverse(root: TokenTree) -> ParseTree:
+    return _traverse(root)[0]
