62683600adac0b702271302634cf344ab206b13f
asiryk <asiryk.net@gmail.com>
2022-05-16 16:08:51 +0300

    feat: implement subtree type for Signature

--- src/model.py
+++ src/model.py
@@ -6,40 +6,48 @@ from conllu.models import Token, Metadata
 
 
 class SigType(Enum):
-    D = "d"  # one ancestor
-    W = "w"  # multiple ancestors
-    N = "n"  # no ancestors
+    D = "d"  # one child
+    W = "w"  # multiple children
+    N = "n"  # no children
 
 
 class Signature:
     type: SigType
-    child_type: Optional[SigType]
+    subtree_type: SigType  # w - subtree grows wide, d - subtree grows depth
     depth: int  # depth counting from the root element
     height: int  # length of the longest downward path to a leaf from that node
     node: Tuple[str, str]  # DEPREL, UPOS
-    children_nodes: List[Tuple[str, str]]
+    child_nodes: List[Tuple[str, str]]
     sentence_id: Optional[str]
 
     def __init__(self, current: 'ParseTree', children: List['ParseTree'], depth: int, height: int, sentence_id: str):
         self.node = (current.get_token("deprel"), current.get_token("upos"))
-        self.children_nodes = [child.signature.node for child in children]
+        self.child_nodes = [child.signature.node for child in children]
         self.depth = depth
         self.height = height
         self.sentence_id = sentence_id
         if len(children) == 0:
             self.type = SigType.N
+            self.subtree_type = SigType.N
         elif len(children) == 1:
             self.type = SigType.D
+            child_type = children[0].signature.type
+            self.subtree_type = SigType.D if child_type in (SigType.N, SigType.D) else SigType.W
         else:
             self.type = SigType.W
+            self.subtree_type = SigType.W
 
     def __str__(self):
         def str_node(n: Tuple[str, str]):
             return f"{n[0]}+{n[1]}"
 
         node = str_node(self.node)
-        children_nodes = ", ".join(str_node(child) for child in self.children_nodes)
-        return f"{self.type.value}({node}, {children_nodes}, {self.depth}, {self.height}, {self.sentence_id})"
+        child_nodes = ", ".join(str_node(child) for child in self.child_nodes)
+        return f"{self.type.value}({node}, " \
+               f"{child_nodes + ', ' if self.type is not SigType.N else ''}" \
+               f"{self.depth}, {self.height}, " \
+               f"{self.subtree_type.value + ', ' if self.type is SigType.D else ''}" \
+               f"{self.sentence_id})"
 
 
 class ParseTree:
@@ -59,7 +67,7 @@ class ParseTree:
         return self.token.get(key)
 
 
-def _traverse(root: TokenTree, depth: int = 0, metadata: Optional[Metadata] = None) -> Tuple[ParseTree, int]:
+def _traverse(root: TokenTree, depth: int, metadata: Optional[Metadata]) -> Tuple[ParseTree, int]:
     metadata = root.metadata if metadata is None else metadata
     children = []
     height = 0
@@ -75,4 +83,4 @@ def _traverse(root: TokenTree, depth: int = 0, metadata: Optional[Metadata] = No
 
 
 def traverse(root: TokenTree) -> ParseTree:
-    return _traverse(root)[0]
+    return _traverse(root, 0, None)[0]
